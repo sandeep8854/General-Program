0acb60b949c1626a724c2c397a257c46ae4b5d41
Sandeep Prajapati <sandeepstructure@gmail.com>
2020-11-15 15:07:14 +0530

    prime

--- General-Program/src/sandeepstructure/com/General_34.java
+++ General-Program/src/sandeepstructure/com/General_34.java
@@ -1,5 +1,31 @@
 package sandeepstructure.com;
 
-public class General_34 {
+import java.util.Scanner;
+
+public class General_34
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Any NUmber ? ?");
+		int number=sc.nextInt();
+		boolean flag=false;
+		int i=2;
+		while(number/2 >=i)
+		{
+			//Condition for nonPrime number
+			if(number%i==0)
+			{
+				flag=true;
+				break;
+			}
+			++i;
+		}
+		if(!flag)
+			System.out.println(number+ " Is A Prime NUmber.");
+		else
+			System.out.println(number+ " Is Not Prime Number.");
+	}
+
 
 }
